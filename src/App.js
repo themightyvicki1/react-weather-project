10bcac4c85ab93b596f7fecdf802038af3f13965
themightyvicki1 <victoria.greer861@gmail.com>
2024-01-10 16:53:52 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather Application</h1>
-      <footer>
-        <a
-          href="https://github.com/themightyvicki1/react-weather-project"
-          title="https://github.com/themightyvicki1/react-weather-project"
-          target="_blank"
-        >
-          Open source
-        </a>{" "}
-        coded by Victoria Greer{" "}
-        <small>
-          <i class="fa-regular fa-heart"></i>
-        </small>
-      </footer>
+      <div className="container">
+        <h1>React Weather Application</h1>
+        <footer>
+          <a
+            href="https://github.com/themightyvicki1/react-weather-project"
+            title="https://github.com/themightyvicki1/react-weather-project"
+            target="_blank"
+          >
+            Open source
+          </a>{" "}
+          coded by Victoria Greer{" "}
+          <small>
+            <i class="fa-regular fa-heart"></i>
+          </small>
+        </footer>
+        <button className="btn btn-primary">Hello</button>
+      </div>
     </div>
   );
 }
