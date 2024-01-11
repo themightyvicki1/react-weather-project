33dfdf889ed42006daad8457c57452cddb3b8b54
themightyvicki1 <victoria.greer861@gmail.com>
2024-01-10 16:23:02 -0800

    added font awesome and google font

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather</h1>
+      <h1>React Weather Application</h1>
+      <footer>
+        <a
+          href="https://github.com/themightyvicki1/react-weather-project"
+          title="https://github.com/themightyvicki1/react-weather-project"
+          target="_blank"
+        >
+          Open source
+        </a>{" "}
+        coded by Victoria Greer{" "}
+        <small>
+          <i class="fa-regular fa-heart"></i>
+        </small>
+      </footer>
     </div>
   );
 }
